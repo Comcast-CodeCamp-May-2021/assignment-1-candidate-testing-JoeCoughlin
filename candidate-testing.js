bf7d9b65771863ba1515f6db90e5a348d25db97d
JoeCoughlin <>
2021-05-14 05:44:42 +0000

    First test

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,58 +1,92 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
+ // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
-let candidateName;
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+ // TODO 1.1a: Define candidateName // 
+ let candidateName = '';
+ // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+ let question = '1) Who was the first American woman in space? ';
+ let correctAnswer = 'Sally Ride';
+ let candidateAnswer;
+ let questions = [ '1) Who was the first American woman in space? ',
+                   '2) True or false: 5000 meters = 5 kilometers. ',
+                   '3) (5 + 3)/2 * 10 = ? ',
+                   '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',
+                   '5) What is the minimum size for the ISS? '];
+ let correctAnswers = ['Sally Ride',
+                       'true',
+                       40,
+                       'Trajectory',
+                       3];
+ let candidateAnswers = [];
 
+   // TODO 1.1b: Ask for candidate's name //
 
-function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+    function askForName() {
+      candidateName = input.question('Enter your name: ');
+      console.log(candidateName + '\n');
+ }
 
-}
+   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+    function askQuestion() {
+      for (let i=0; i < questions.length; i++){
+        candidateAnswers[i] = input.question("\n" + questions[i]);
+      }
+ }
 
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+    function gradeQuiz() {
 
+   let correct = 0;
+   for (i = 0; i < correctAnswers.length; i++){
+     console.log(questions[i] + "\n");
+     console.log("Your answer: " + candidateAnswers[i]);
+     console.log("Correct answer: " + correctAnswers[i] + "\n")
+    if (candidateAnswers[i].toString().toLowerCase() === correctAnswers[i].toString().toLowerCase()){
+     correct++
+   }
+   }
+   
+let grade = (correct / (questions.length) *100);
 
-}
+    console.log(`>>> Overall Grade: ${grade}% (${correct} out of ${questions.length} answers correct) <<<`);
+    
+    if (grade >= 80){
+      console.log('>>> Status: PASSED <<<');
+    }
+    else{
+      console.log('>>> Status: FAILED <<<');
+    }
+    
+   if (grade >= 80){
+    console.log (candidateName + " passed with a score of " + grade + "%");
+  }
+  else {
+    console.log (candidateName + " failed with a score of " + grade + "%");
+  }
+    return grade;
+   }
 
-function gradeQuiz(candidateAnswers) {
+ 
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+ function runProgram() {
+   askForName();
+   // TODO 1.1c: Ask for candidate's name //
 
+   askQuestion();
+   gradeQuiz(this.candidateAnswers);
+ }
 
-  let grade;
-  
-
-  return grade;
-}
-
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
-}
-
-// Don't write any code below this line //
-// And don't change these or your program will not run as expected //
-module.exports = {
-  candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
-  questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
-  runProgram: runProgram
-};
+ // Don't write any code below this line //
+ // And don't change these or your program will not run as expected //
+ module.exports = {
+   candidateName: candidateName,
+   question: question,
+   correctAnswer: correctAnswer,
+   candidateAnswer: candidateAnswer,
+   questions: questions,
+   correctAnswers: correctAnswers,
+   candidateAnswers: candidateAnswers,
+   gradeQuiz: gradeQuiz,
+   runProgram: runProgram
+ };
